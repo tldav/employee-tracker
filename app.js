01658891b83eddc5d3ff4e48263db86283111d0e
Neverender <tldavis09@yahoo.com>
2020-03-15 19:59:00 -0500

    Added initial prompts with inquirer

--- app.js
+++ app.js
@@ -8,3 +8,65 @@ const connection = mysql.createConnection({
 	password: "",
 	database: "employeesDB"
 });
+
+connection.connect((err) => {
+	if (err) throw err;
+
+	runApp();
+});
+
+function runApp() {
+	inquirer
+		.prompt({
+			name: "action",
+			type: "list",
+			message: "What would you like to do?",
+			choices: [
+				"View departments",
+				"View roles",
+				"View employees",
+				"Add a department",
+				"Add a role",
+				"Add an employee",
+				"Update existing employee"
+			]
+		})
+		.then((answer) => {
+			switch (answer.action) {
+				case "View departments":
+					break;
+				case "View roles":
+					break;
+				case "View employees":
+					break;
+				case "Add a department":
+					break;
+				case "Add a role":
+					break;
+				case "Add an employee":
+					break;
+				case "Update existing employee":
+					break;
+			}
+		});
+}
+
+// What would you like to do?
+//     View departments
+//     View roles
+//     View employees
+//     Add department
+//     Add role
+//     Add Employee
+//     Update Employee
+
+/** Optional
+ *      Update employee manager
+ *      view employees by manager
+ *      Delete departments, roles, and employees
+ *      View total utilized budget of a dept -- combined salary of dept
+ */
+
+/** Included in GIF but not README
+ *      View all employees by dept
+ */
